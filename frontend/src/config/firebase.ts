49df51f3a2d98edfe6d420dfbbdb2ebd0e28cc99
Jai Krishna <jai.k@zluri.com>
2025-01-28 16:08:37 +0530

    fixed

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -1,17 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// Load environment variables from .env file
-require('dotenv').config();
-
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDAx1zStV4b4q6moGxdwYuX-ea_3G5FTwI",
+    authDomain: "transaction-valley.firebaseapp.com",
+    projectId: "transaction-valley",
+    storageBucket: "transaction-valley.firebasestorage.app",
+    messagingSenderId: "921992032275",
+    appId: "1:921992032275:web:9396a3799d9b1ba4480fe6",
+    measurementId: "G-47HJ63TVCQ"
 };
 
 const app = initializeApp(firebaseConfig);
